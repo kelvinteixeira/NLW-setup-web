7de552a23509ac706bce41447627e877831c0d4f
kesantos <kesantos@paschoalotto.com.br>
2023-01-18 11:27:02 -0300

    feat(SummayTable): Criado componente SummaryTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // import { Habit } from './component/Habit'
 
 import { Header } from "./component/Header";
+import { SummaryTable } from "./component/SummaryTable";
 
 
 export function App() {
@@ -9,6 +10,7 @@ export function App() {
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
         <Header />
+        <SummaryTable />
       </div>
     </div>
   )
