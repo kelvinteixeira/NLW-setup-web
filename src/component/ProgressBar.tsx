fe2d9cc2ccd325c7def97bf2c943bd13e11746f0
kesantos <kesantos@paschoalotto.com.br>
2023-01-18 23:26:19 -0300

    feat(Popover): Criado componente popover

---
+++ src/component/ProgressBar.tsx
@@ -0,0 +1,20 @@
+type ProgressBarProps = {
+  progress: number
+}
+
+export function ProgressBar(props: ProgressBarProps) {
+
+  return (
+    <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
+      <div
+        role="progressbar"
+        aria-label="Progesso de hábitos completados no dia"
+        aria-valuenow={props.progress}
+        className='h-3 rounded-xl bg-violet-600'
+        style={{
+          width: `${props.progress}%`
+        }}
+      />
+    </div>
+  )
+}
